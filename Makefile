8abf2d57fd16ebd501922fcda4ab2264c670f01c
Taki-do <malbecktao@gmail.com>
2025-05-12 11:09:32 +0200

    la base

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME = cub3D
+PATH_LIBFT = libft/
+LIBFT_LIB = $(PATH_LIBFT)libft.a
+SRC = 
+SRC_BONUS = 
+OBJ = $(SRC:.c=.o)
+OBJ_BONUS = $(SRC_BONUS:.c=.o)
+CC = gcc
+FLAGS = -Wall -Wextra -Werror
+
+all: $(LIBFT_LIB) $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(FLAGS) $(OBJ) $(LIBFT_LIB) -o $(NAME)
+
+%.o: %.c
+	$(CC) $(FLAGS) -c $< -o $@
+
+$(LIBFT_LIB):
+	make -C $(PATH_LIBFT)
+
+bonus: $(LIBFT_LIB) $(OBJ_BONUS)
+	$(CC) $(FLAGS) $(OBJ_BONUS) $(LIBFT_LIB) -o $(NAME)
+
+clean:
+	/bin/rm -f $(OBJ) $(OBJ_BONUS)
+	make -C $(PATH_LIBFT) clean
+
+fclean: clean
+	/bin/rm -f $(NAME)
+	make -C $(PATH_LIBFT) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taomalbe <taomalbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 11:06:16 by taomalbe          #+#    #+#             */
+/*   Updated: 2025/05/12 11:08:13 by taomalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../minilibx-linux/mlx.h"
+# include "../libft/libft.h"
+# include <math.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+#endif
