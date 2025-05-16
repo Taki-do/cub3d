6eb84bba3983c1f61947a9e5949017035c49c961
Takido-NFT <malbecktao@gmail.com>
2025-05-16 11:11:49 +0200

    Je note : s'acheter des neuronnes la prochaine fois

--- srcs/raycasting/input.c
+++ srcs/raycasting/input.c
@@ -6,42 +6,11 @@
 /*   By: taomalbe <taomalbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 19:10:29 by taomalbe          #+#    #+#             */
-/*   Updated: 2025/05/15 19:11:19 by taomalbe         ###   ########.fr       */
+/*   Updated: 2025/05/16 11:06:54 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
-
-#define mapWidth 24
-#define mapHeight 24
-
-int worldMap[mapWidth][mapHeight]=
-{
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
 
 int	on_press(int keycode, t_data *data)
 {
@@ -88,33 +57,33 @@ int	control_input(t_data *data)
 	double rotSpeed = 0.01f;
 	if (data->keys.w)
 	{
-		if(worldMap[(int)(data->posX + data->dirX * moveSpeed)][(int)(data->posY)] == 0)
+		if(data->config.map_lines[(int)(data->posY)][(int)(data->posX + data->dirX * moveSpeed)] == '0')
 			data->posX += data->dirX * moveSpeed;
-		if(worldMap[(int)(data->posX)][(int)(data->posY + data->dirY * moveSpeed)] == 0)
+		if(data->config.map_lines[(int)(data->posY + data->dirY * moveSpeed)][(int)(data->posX)] == '0')
 			data->posY += data->dirY * moveSpeed;
 	}
 	//move backwards if no wall behind you
 	if (data->keys.s)
 	{
-		if(worldMap[(int)(data->posX - data->dirX * moveSpeed)][(int)(data->posY)] == 0)
+		if(data->config.map_lines[(int)(data->posY)][(int)(data->posX - data->dirX * moveSpeed)] == '0')
 			data->posX -= data->dirX * moveSpeed;
-		if(worldMap[(int)(data->posX)][(int)(data->posY - data->dirY * moveSpeed)] == 0)
+		if(data->config.map_lines[(int)(data->posY - data->dirY * moveSpeed)][(int)(data->posX)] == '0')
 			data->posY -= data->dirY * moveSpeed;
 	}
 	//move to the right
 	if (data->keys.d)
 	{
-		if(worldMap[(int)(data->posX + data->planeX * moveSpeed)][(int)(data->posY)] == 0)
+		if(data->config.map_lines[(int)(data->posY)][(int)(data->posX + data->planeX * moveSpeed)] == '0')
 			data->posX += data->planeX * moveSpeed;
-		if(worldMap[(int)(data->posX)][(int)(data->posY + data->planeY * moveSpeed)] == 0)
+		if(data->config.map_lines[(int)(data->posY + data->planeY * moveSpeed)][(int)(data->posX)] == '0')
 			data->posY += data->planeY * moveSpeed;
 	}
 	//move to the left
 	if (data->keys.a)
 	{
-		if(worldMap[(int)(data->posX - data->planeX * moveSpeed)][(int)(data->posY)] == 0)
+		if(data->config.map_lines[(int)(data->posY)][(int)(data->posX - data->planeX * moveSpeed)] == '0')
 			data->posX -= data->planeX * moveSpeed;
-		if(worldMap[(int)(data->posX)][(int)(data->posY - data->planeY * moveSpeed)] == 0)
+		if(data->config.map_lines[(int)(data->posY - data->planeY * moveSpeed)][(int)(data->posX)] == '0')
 			data->posY -= data->planeY * moveSpeed;
 			
 	}
@@ -140,3 +109,64 @@ int	control_input(t_data *data)
 	render(data);
 	return (0);
 }
+
+/*
+int	control_input(t_data *data)
+{
+	double moveSpeed = 0.03f;
+	double rotSpeed = 0.01f;
+	if (data->keys.w)
+	{
+		if(data->config.map_lines[(int)(data->posX + data->dirX * moveSpeed)][(int)(data->posY)] == '0')
+			data->posX += data->dirX * moveSpeed;
+		if(data->config.map_lines[(int)(data->posX)][(int)(data->posY + data->dirY * moveSpeed)] == '0')
+			data->posY += data->dirY * moveSpeed;
+	}
+	//move backwards if no wall behind you
+	if (data->keys.s)
+	{
+		if(data->config.map_lines[(int)(data->posX - data->dirX * moveSpeed)][(int)(data->posY)] == '0')
+			data->posX -= data->dirX * moveSpeed;
+		if(data->config.map_lines[(int)(data->posX)][(int)(data->posY - data->dirY * moveSpeed)] == '0')
+			data->posY -= data->dirY * moveSpeed;
+	}
+	//move to the right
+	if (data->keys.d)
+	{
+		if(data->config.map_lines[(int)(data->posX + data->planeX * moveSpeed)][(int)(data->posY)] == '0')
+			data->posX += data->planeX * moveSpeed;
+		if(data->config.map_lines[(int)(data->posX)][(int)(data->posY + data->planeY * moveSpeed)] == '0')
+			data->posY += data->planeY * moveSpeed;
+	}
+	//move to the left
+	if (data->keys.a)
+	{
+		if(data->config.map_lines[(int)(data->posX - data->planeX * moveSpeed)][(int)(data->posY)] == '0')
+			data->posX -= data->planeX * moveSpeed;
+		if(data->config.map_lines[(int)(data->posX)][(int)(data->posY - data->planeY * moveSpeed)] == '0')
+			data->posY -= data->planeY * moveSpeed;
+			
+	}
+	if (data->keys.left) //fleche gauche
+	{
+		double oldDirX = data->dirX;
+		data->dirX = data->dirX * cos(rotSpeed) - data->dirY * sin(rotSpeed);
+		data->dirY = oldDirX * sin(rotSpeed) + data->dirY * cos(rotSpeed);
+		double oldPlaneX = data->planeX;
+		data->planeX = data->planeX * cos(rotSpeed) - data->planeY * sin(rotSpeed);
+		data->planeY = oldPlaneX * sin(rotSpeed) + data->planeY * cos(rotSpeed);
+	}
+	if (data->keys.right) //fleche droite
+	{
+		//both camera direction and camera plane must be rotated
+		double oldDirX = data->dirX;
+		data->dirX = data->dirX * cos(-rotSpeed) - data->dirY * sin(-rotSpeed);
+		data->dirY = oldDirX * sin(-rotSpeed) + data->dirY * cos(-rotSpeed);
+		double oldPlaneX = data->planeX;
+		data->planeX = data->planeX * cos(-rotSpeed) - data->planeY * sin(-rotSpeed);
+		data->planeY = oldPlaneX * sin(-rotSpeed) + data->planeY * cos(-rotSpeed);
+	}
+	render(data);
+	return (0);
+}
+*/
